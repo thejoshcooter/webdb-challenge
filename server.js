9df366af37bf869a4678ec79e2cb22b5aae20c77
josh cooter <thejoshcooter@gmail.com>
2019-06-07 10:39:46 -0500

    add post endpoints to projects and actions

--- server.js
+++ server.js
@@ -12,6 +12,37 @@ const server = express();
 // body parser
 server.use(express.json());
 
+// add a project
+server.post('/api/projects', (req, res) => {
+    if(req.body) {
+        db('projects')
+        .insert(req.body)
+        .then(id => {
+            res.status(201).json({ message: 'project added' });
+        })
+        .catch(err => {
+            res.status(400).json({ error: 'error adding project' })
+        })
+    }
+});
+
+// add an action
+server.post('/api/actions', (req, res) => {
+    if(req.body) {
+        db('actions')
+        .insert(req.body)
+        .then(id => {
+            res.status(201).json({ message: 'action added' });
+        })
+        .catch(err => {
+            res.status(400).json({ error: 'error adding action' })
+        })
+    }
+});
+
+// get projects by id
+
+
 
 // default fallback
 server.get('/', (req, res) => {
